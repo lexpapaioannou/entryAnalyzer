fbc1d2d0028ecb82fc563d32aa936657b1bf8fee
Lex Papaioannou <apapaioannou@luc.edu>
2017-11-24 15:28:27 -0600

    Started new git

---
+++ EntryAnalyzer.java
@@ -0,0 +1,27 @@
+import java.io.Reader;
+import java.util.Scanner;
+import java.io.File;
+
+	/*PSEUDO CODE:
+		1) Check if the file path exists, otherwise ask again
+	*/
+
+public class EntryAnalyzer {
+	public static void main(String [] args) {
+		EntryAnalyzer entryAnalyzer = new EntryAnalyzer();
+		entryAnaylzer.start();	
+	}
+
+	void start() {
+	
+	}
+
+	selectFile() { //Checks if the file name is valid and then converts it into an element (string or array?)
+		Scanner scanner = new scanner(System.in);
+		System.out.println("Please enter the file name: "+\n);
+		while (true) {
+			File fileName = scanner.next();
+			
+		}
+	}
+}
