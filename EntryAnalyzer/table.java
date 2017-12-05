5e76004ff34248b8624008af183eae075d89f2b2
Lex Papaioannou <apapaioannou@luc.edu>
2017-12-04 22:26:08 -0600

    Several tweaks that made package compilable

---
+++ EntryAnalyzer/table.java
@@ -0,0 +1,28 @@
+package EntryAnalyzer;
+
+public class table {
+	String label;
+	int hits;
+
+	public table (String label, int hits) {
+		this.label = label;
+		this.hits = hits;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+
+	public int getHits() {
+		return hits;
+	}
+
+	public void setLabel() {
+		this.label = label;
+	}
+
+	public void setHits() {
+		this.hits = hits;
+	}
+
+}
